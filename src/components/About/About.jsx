196758b522fd2e15d330e8912eed9f5767f513e9
Ashok-Bhatt <ashokbhatt2048@gmail.com>
2025-06-28 11:45:11 +0530

    Delete unwanted Pagination component and fixed Ugly About Us Component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,14 +4,14 @@ import "./About.css"
 function About() {
   return (
     <div className="about-section">
-      <h2 className="about-us-heading">About Us</h2>
-      <div className="about-us-content">
-          <p>Hello, welcome to Pokedex Lite</p>
-          <p>Know anything about your favourite pokemon</p>
-          <p>Why are you waiting grab your pokedex and start exploring the world of pokemons</p>
-          <p style={{alignSelf : "flex-end", marginTop : "60px"}}>Developed by Ashok Bhatt</p>
-      </div>
+    <h2 className="about-us-heading">About Us</h2>
+    <div className="about-us-content">
+        <p>Welcome to <strong>Pokédex Lite</strong> — your simplified gateway to the world of Pokémon!</p>
+        <p>Discover detailed information about your favorite Pokémon, from their unique abilities to their evolutionary journeys.</p>
+        <p>Why wait? Grab your Pokédex and embark on your own adventure to explore the amazing Pokémon universe.</p>
+        <p style={{ alignSelf: "flex-end", marginTop: "60px" }}>Crafted with passion by Ashok Bhatt</p>
     </div>
+  </div>
   )
 }
 
